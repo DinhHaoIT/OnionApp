f03a23f7a7686bf8244d2ed3590be1bd83d308da
DinhHaoIT <nguyendinhhao.it@gmail.com>
2024-01-31 12:25:18 +0700

    Add project files.

---
+++ Persistence/AppDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Application;
+using Domain;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persistence
+{
+    public class AppDbContext : DbContext, IAppDbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<Student> Students { get; set; }
+
+        public async Task<int> SaveChangesAsync()
+        {
+            return await base.SaveChangesAsync();
+        }
+    }
+}
